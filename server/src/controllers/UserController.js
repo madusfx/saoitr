6c59dedc85e7cb52ad92f98bbfd1231762fdfc7a
madusfx <mariaesfreitas@gmail.com>
2023-05-16 22:13:31 -0300

    feat: creating the login route

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json');
 
 const UserModel = require('../models/User');
 
 const router = express.Router();
+
+const generateToken = (user = {}) => {
+  return jwt.sign({
+    id: user.id,
+    name: user.name
+  }, authConfig.secret, {
+    expiresIn: 3600
+  });
+}
 
 router.post('/', async (req, res) => {
   const { name, email, password } = req.body;
@@ -22,12 +34,41 @@ router.post('/', async (req, res) => {
     return res.json({
       error: false,
       message: "Registered with success!",
-      data: User
+      data: User,
+      token: generateToken(User)
     });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Erro no servidor" });
   }
 });
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await UserModel.findOne({ email }).select("+password");
+
+  if (!user) {
+    return res.status(400).json({
+      error: true,
+      message: 'Usuário não cadastrado.'
+    })
+  }
+
+  if (!await bcryptjs.compare(password, user.password)) {
+    return res.status(400).send({
+      error: true,
+      message: 'Senha inválida'
+    })
+  };
+
+  user.password = undefined;
+
+  return res.json({
+    user,
+    token: generateToken(user)
+  });
+
+})
 
 module.exports = router;
