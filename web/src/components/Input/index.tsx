4767e4277c609964b859cbfbdf27342992b8a892
madusfx <mariaesfreitas@gmail.com>
2023-05-18 17:01:30 -0300

    feat: connecting server with web on register

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -5,14 +5,15 @@ import * as S from './styles';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   placeholder?: string;
+  onChange?: any;
 }
 
-const Input = ({ label, placeholder }: InputProps) => {
+const Input = ({ label, placeholder, onChange }: InputProps) => {
 
   return (
     <S.Container >
       <label>{label}</label>
-      <input placeholder={placeholder} />
+      <input placeholder={placeholder} onChange={onChange} />
     </S.Container>
   )
 };
--- web/src/pages/cadastro/index.tsx
+++ web/src/pages/cadastro/index.tsx
@@ -3,21 +3,50 @@ import { useRouter } from 'next/router';
 import * as S from './styles';
 import Button from '@/components/Button';
 import { useState } from 'react';
+import api from '@/services/api';
+import axios from 'axios';
+import Input from '@/components/Input';
 
 export default function Cadastro() {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const router = useRouter();
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const userData = {
+      name: name,
+      email: email,
+      password: password
+    };
+    axios
+    api.post("/users", userData)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log("server responded");
+        } else if (error.request) {
+          console.log("network error");
+        } else {
+          console.log(error);
+        }
+      });
+  };
 
   return (
     <S.Card>
       <S.Title>
         Cadastro
       </S.Title>
-      <form>
-        <S.Input type='text' placeholder='Nome' value={email} />
-        <S.Input type='email' placeholder='E-mail' />
-        <S.Input type='password' placeholder='Senha' />
-        <Button>
+      <form onSubmit={handleSubmit} >
+        <Input type='text' placeholder='Nome' value={name} onChange={(e: any) => setName(e.target.value)} />
+        <Input type='email' placeholder='E-mail' value={email} onChange={(e: any) => setEmail(e.target.value)} />
+        <Input type='password' placeholder='Senha' value={password} onChange={(e: any) => setPassword(e.target.value)} />
+        <Button type='submit'>
           Cadastrar
         </Button>
         <S.Register>
