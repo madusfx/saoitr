884e72dbf9df01131b5e22b4febce13202808b87
madusfx <mariaesfreitas@gmail.com>
2023-05-18 10:17:11 -0300

    feat: adding jwt authentication

---
+++ server/src/middlewares/authenticate.js
@@ -0,0 +1,49 @@
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({
+      error: true,
+      message: "Token não fornecido"
+    })
+  }
+
+  const parts = authHeader.split(" ");
+
+  if (parts.length !== 2) {
+    return res.status(401).json({
+      error: true,
+      message: "Invalid Token Type"
+    })
+  }
+
+  const [scheme, token] = parts;
+
+  if (scheme.indexOf("Bearer") !== 0) {
+    return res.status(401).json({
+      error: true,
+      message: "Token unformatted"
+    })
+  }
+
+  jwt.verify(token, authConfig.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        error: true,
+        message: "Token invalid/expired"
+      })
+    }
+
+    req.userLogged = decoded;
+
+    console.log(err);
+    console.log(decoded);
+
+    return next();
+  })
+
+  next();
+}
--- server/src/server.js
+++ server/src/server.js
@@ -3,10 +3,14 @@ const express = require('express');
 const database = require('../src/database');
 
 const UserController = require('./controllers/UserController');
+const AdminController = require('./controllers/AdminController');
+const authenticateMiddleware = require('./middlewares/authenticate');
+
 const app = express();
 
 app.use(express.json());
 app.use("/users", UserController);
+app.use("/admin", authenticateMiddleware, AdminController);
 
 app.listen(3001, () => {
   console.log('Server is runing');
