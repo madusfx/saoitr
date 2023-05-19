c8e67afd702a405fb3353413e200d05dc033eadf
madusfx <mariaesfreitas@gmail.com>
2023-05-19 19:47:25 -0300

    feat: connecting server with web on login

--- web/src/pages/login/index.tsx
+++ web/src/pages/login/index.tsx
@@ -1,32 +1,51 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
-// import api from "../../services/api";
-
-import * as S from './styles';
+import md5 from 'md5';
+import api from "../../services/api";
+import { login } from '@/services/auth';
 import Button from '@/components/Button';
 import Input from '@/components/Input';
 
+import * as S from './styles';
+import axios from 'axios';
+
 export default function Login() {
-  const [users, setUsers] = useState([]);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const router = useRouter();
 
-  // useEffect(() => {
-  //   api.get("users").then(({ data }) => {
-  //     setUsers(data.users);
-  //   })
-  //   console.log(users);
-
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const userData = {
+      email: email,
+      password: password.length > 0 ? md5(password) : null
+    };
+    axios
+    api.post("/login", userData)
+      .then((response) => {
+        login(response.data.token);
+        console.log(response);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log("server responded");
+        } else if (error.request) {
+          console.log("network error");
+        } else {
+          console.log(error);
+        }
+      });
+  };
 
   return (
     <S.Card>
       <S.Title>
         Login
       </S.Title>
-      <form>
-        <Input type='email' placeholder='E-mail' />
-        <Input type='password' placeholder='Senha' />
+      <form onSubmit={handleSubmit}>
+        <Input type='email' placeholder='E-mail' value={email} onChange={(e: any) => setEmail(e.target.value)} />
+        <Input type='password' placeholder='Senha' value={password} onChange={(e: any) => setPassword(e.target.value)} />
         <Button type='submit'>
           Entrar
         </Button>
