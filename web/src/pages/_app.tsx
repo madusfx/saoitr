c744a38d5d52d7ad7e27f349edb4a3073dae9556
madusfx <mariaesfreitas@gmail.com>
2023-05-22 22:19:57 -0300

    feat: adding montserrat font

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import GlobalStyle from '@/styles/global';
 import type { AppProps } from 'next/app';
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ['latin'], weight: '400' });
 
 export default function App({ Component, pageProps }: AppProps) {
+
   return (
-    <>
+    <main className={montserrat.className}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </main>
   )
 }
