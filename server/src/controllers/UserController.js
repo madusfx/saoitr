83ffcb8a7df4a84ea21afdfb7598c12a110151a6
madusfx <mariaesfreitas@gmail.com>
2023-06-15 21:47:47 -0300

    feat: adding get for user

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth.json');
 
 const UserModel = require('../models/User');
+const UserSchema = require('../models/User');
 
 const router = express.Router();
 
@@ -19,8 +20,6 @@ router.post('/users', async (req, res) => {
   const { name, email, password } = req.body;
   const userId = (await UserModel.countDocuments()) + 1
 
-  console.log('userId', userId)
-
   if (await UserModel.findOne({ email })) {
     return res.status(400).json({ error: true, message: 'E-mail já cadastrado' });
   }
@@ -34,7 +33,7 @@ router.post('/users', async (req, res) => {
     });
     await User.save();
     User.password = undefined;
-    return res.json({
+    return res.status(201).json({
       name: User.name,
       email: User, email,
       id: userId,
@@ -81,26 +80,19 @@ router.post('/logout', async (req, res) => {
   const { id } = req.body;
   const query = { id: id };
   const user = await UserModel.findOne(query).exec();
+
   if (!user) {
     return res.status(401).json({
       message: "Usuário não encontrado"
     })
   };
 
-  const secret = process.env.JWT_SECRET;
   const bearerHeader = req.headers['authorization'];
   const bearerToken = bearerHeader.split(' ')[1];
-  const decoded = jwt.verify(bearerToken, secret);
-
-  // if (decoded.id != user.id) {
-  //   return res.status(401).json({
-  //     message: "Essas credenciais não correspondem aos nossos registros.  -- ID NÃO CORRESPONDE AO TOKEN"
-  //   })
-  // };
 
   if (blacklist.find((item) => item == bearerToken)) {
     return res.status(401).json({
-      message: "Essas credenciais não correspondem aos nossos registros.  -- TOKEN INVÁLIDO"
+      message: "Essas credenciais não correspondem aos nossos registros."
     })
   };
 
@@ -115,12 +107,52 @@ router.put('/users/:userId', async (req, res) => {
   const { userId } = req.params;
   const query = { id: userId };
   const { name, email, password } = req.body;
-  const user = !password ? await UserModel.findOneAndUpdate(query, { name, email }, { new: true }) : await UserModel.findOneAndUpdate(query, { name, email, password }, { new: true });
+
+  const userEmail = await UserModel.findOne({ email })
+  if (userEmail && userEmail.id != userId) {
+    return res.status(400).json({ error: true, message: 'E-mail já cadastrado' });
+  }
+
+  try {
+    if (!password) {
+      console.log("Usuário (nome e email) foi atualizado!");
+      await UserModel.findOneAndUpdate(query, { name, email }, { new: true })
+    }
+    else {
+      console.log("Usuário (nome, email e senha) foi atualizado!");
+      await UserModel.findOneAndUpdate(query, { name, email, password }, { new: true })
+    }
+  }
+  catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Erro no servidor" });
+  }
+
+  const user = await UserModel.findOne(query);
 
   res.status(200).json({
-    id: user.id,
     name: user.name,
     email: user.email,
-  })
+    id: user.id,
+    message: "Usuário atualizado com sucesso"
+  });
 })
+
+router.get('/users/:userId', async (req, res) => {
+  const { userId } = req.params;
+  const query = { id: userId };
+
+  try {
+    const user = await UserModel.findOne(query);
+    console.log("Usuário encontrado!");
+    return res.status(200).send(user);
+  }
+  catch (err) {
+    console.log("Usuário não encontrado!");
+    return res.status(400).json({
+      message: "Usuário não encontrado!"
+    })
+  }
+});
+
 module.exports = router;
