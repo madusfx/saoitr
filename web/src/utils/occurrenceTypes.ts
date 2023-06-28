27a5efe4a5fd2509731b1e9c70fda0a8e8985ef7
madusfx <mariaesfreitas@gmail.com>
2023-06-28 20:50:19 -0300

    feat: creating occurrences types

---
+++ web/src/utils/occurrenceTypes.ts
@@ -0,0 +1,12 @@
+export const OccurencesTypes = [
+  { id: 1, name: "Atropelamento" },
+  { id: 2, name: "Deslizamento" },
+  { id: 3, name: "Colisão frontal" },
+  { id: 4, name: "Capotagem" },
+  { id: 5, name: "Saída de pista" },
+  { id: 6, name: "Batida em objeto fixo" },
+  { id: 7, name: "Veículo avariado" },
+  { id: 8, name: "Colisão com motocicletas" },
+  { id: 9, name: "Colisão no mesmo sentido ou transversal" },
+  { id: 10, name: "Construção" }
+];
